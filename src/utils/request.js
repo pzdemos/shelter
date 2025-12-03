d5d6a39480fdcef553a0ea39ea1e9637bc7fbfd7
zhaojiu <1903301895@qq.com>
2025-12-03 23:35:09 +0800

    feat:提升deno部署兼容性

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { message } from 'antd';
 
 const request = axios.create({
-  baseURL: '/api',
+  baseURL:  window.location.origin.includes('localhost') ? 'http://localhost:5173/api' : 'https://z-gemini.deno.dev/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
